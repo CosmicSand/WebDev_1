aa2a612c1bd4779b0c2db2853948e8a107a13da1
CosmicSand <skyjlen@gmail.com>
2025-03-24 18:25:02 +0200

    done

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener("click", (e) => {
+  if (!e.target.matches("button")) return;
+  const button = e.target;
+  if (button.innerText === "Expand") {
+    console.log("hereee");
+    button.innerText = "Collapse";
+    const divToSet = button.closest("div").nextElementSibling;
+    divToSet.classList.add("show");
+  } else {
+    button.innerText = "Expand";
+    const divToReplace = button.closest("div").nextElementSibling;
+    divToReplace.classList.remove("show");
+  }
+});
