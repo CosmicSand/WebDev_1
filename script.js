9f4c1e90c8441155fa4168b2b7434764b826cb81
CosmicSand <skyjlen@gmail.com>
2025-03-24 18:31:06 +0200

    done

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ document.addEventListener("click", (e) => {
   if (!e.target.matches("button")) return;
   const button = e.target;
   if (button.innerText === "Expand") {
-    console.log("hereee");
     button.innerText = "Collapse";
     const divToSet = button.closest("div").nextElementSibling;
     divToSet.classList.add("show");
